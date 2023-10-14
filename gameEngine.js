a4ed831bcd976ae9d5ff52481c1096bcdf08b8ea
lmnitzsche <logannitzsche1@gmail.com>
2023-10-14 15:44:46 -0500

    working on game engine

--- gameEngine.js
+++ gameEngine.js
@@ -3,17 +3,47 @@ const selectedCardSources = params.getAll('card');
 const imageContainer = document.querySelector('.image-container');
 const opponentContainer = document.querySelector('.opponent-container');
 let count = 1;
+let positionSpot = 1;
+let pCount = 0;
 
 selectedCardSources.forEach(source => {
+    const cardContainer = document.createElement('div');
+    cardContainer.className = 'card-container';
+
     const card = document.createElement('img');
     card.src = source;
     card.className = 'image';
+
+    const health = document.createElement('div');
+    health.className = 'health'; 
+    const healthValue = 100;
+    health.textContent = 'Health: ${healthValue}';
+
+    const armor = document.createElement('div');
+    armor.className = 'armor';
+    const armorValue = 100;
+    armor.textContent = 'Armor: ${armorValue}';
+
+    const position = document.createElement('div');
+    position.className = 'position';
+    pCount++;
+
+    if(pCount % 2 == 0 && pCount != 0) {
+        positionSpot++;
+    }
+    position.textContent = 'Position: ${positionSpot}';
+
+
+    cardContainer.appendChild(card);
+    cardContainer.appendChild(health);
+    cardContainer.appendChild(armor);
+    cardContainer.appendChild(position);
     
     if(count % 2 == 1) {
-        imageContainer.appendChild(card);
+        imageContainer.appendChild(cardContainer);
     }
     else {
-        opponentContainer.appendChild(card);
+        opponentContainer.appendChild(cardContainer);
     }
     count++;
 });
